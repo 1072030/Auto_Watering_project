fba560c3650769c2460e48192510b17a0e472eb5
1072030 <229jdu@gmail.com>
2022-11-14 15:01:57 +0800

    update

---
+++ Auto_Watering_project.ino
@@ -0,0 +1,15 @@
+#define Pin_Water 1
+#define Pin_Light 2
+#define Pin_Bright 3
+#define Pin_LCD 4
+#define Pin_LCD2 5
+#define Pin_Soil 6
+void setup() {
+  // put your setup code here, to run once:
+  Serial.print("9600");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
