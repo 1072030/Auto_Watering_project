d9836f26e89e9691164c2e5842c5907f264b2a99
1072030 <229jdu@gmail.com>
2022-11-15 11:07:54 +0800

    setup write detail

--- main.ino
+++ main.ino
@@ -7,6 +7,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.print("9600");
+  //Setup blynk
+  //Setup relay
+  //Setup watering timing
+  //Setup light timing
+  //Setup light turn on/off
+  //Setup LCD show
 }
 
 void loop() {
